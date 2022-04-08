391435ba2b9e702dbc074ac894eda8a022b4103f
onlyhappiness <onlyhappiness96@gmail.com>
2022-04-08 16:08:21 +0900

    [UPDATE] Before react-query

--- app/src/App.js
+++ app/src/App.js
@@ -1,33 +1,13 @@
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import React from 'react'
 
-const queryClient = new QueryClient();
+import Characters from './components/Characters'
 
-function Example() {
-  const { isLoading, error, data } = useQuery('repoData', () =>
-    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
-      (res) => res.json()
-    )
-  );
-
-  if (isLoading) return 'Loading...';
-
-  if (error) return 'An error has occurred: ' + error.message;
-
+function App() {
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>
-      <strong>✨ {data.stargazers_count}</strong>
-      <strong>🍴 {data.forks_count}</strong>
+    <div className='App'>
+      <Characters />
     </div>
-  );
+  )
 }
 
-export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Example />
-    </QueryClientProvider>
-  );
-}
+export default App
